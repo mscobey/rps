1d12cd884df0a7236184a8da81ad58314935f201
mscobey <scobey.mark@gmail.com>
2023-03-20 18:13:43 -0500

    game with ui

--- rps/rockpapercissors.js
+++ rps/rockpapercissors.js
@@ -1,23 +1,53 @@
-function getComptuterChoice(){
+function getComputerChoice(){
     let choices=['rock','paper','scissors'];
     return choices[Math.floor(Math.random()*3)];
 }
 //console.log(getComptuterChoice());
+let playerWins=0;
+let computerWins=0;
 
-function playRound(playerSelection,computerSelection){
-    let winner = 0;
-    computerSelection=='rock'&&playerSelection.toUpperCase()=='SCISSORS' ? winner-=1 : computerSelection=='rock'&&playerSelection.toUpperCase()=='PAPER' ? winner+=1 : winner += 0;
-    computerSelection=='paper'&&playerSelection.toUpperCase()=='SCISSORS' ? winner+=1 : computerSelection=='paper'&&playerSelection.toUpperCase()=='ROCK' ? winner-=1 : winner += 0;
-    computerSelection=='scissors'&&playerSelection.toUpperCase()=='ROCK' ? winner+=1 : computerSelection=='scissors'&&playerSelection.toUpperCase()=='PAPER' ? winner-=1 : winner += 0;
-    return winner == 1 ? `You won! ${playerSelection} beats ${computerSelection}!` :
-     winner==-1 ? `You lost :( ${computerSelection} beats ${playerSelection}`:
-      `Draw! ${playerSelection} is the same as ${computerSelection}`;
-}
-//console.log(playRound('ROCK','scissors'));
 
-function game(){
-    for(let i=0;i<5;i++){
-        let p = prompt('Rock, paper, or scissors?');
-        console.log(playRound(p,getComptuterChoice()));
+function playRound(event){
+    let playerSelection=event.target.id;
+    let computerSelection=getComputerChoice();
+    let winner = '';
+    document.getElementById("winner").innerHTML='';
+
+    if(computerSelection=='rock'){
+        playerSelection=='rock'?winner='draw':playerSelection=='paper'?winner='Player':winner='Computer';
+    }
+    else if(computerSelection=='paper'){
+        playerSelection=='rock'?winner='Computer':playerSelection=='paper'?winner='draw':winner='Player';
+    }
+    else{
+        playerSelection=='rock'?winner='Player':playerSelection=='paper'?winner='Computer':winner='draw';
+    }
+    if(winner!='draw'){
+        document.getElementById('gameResults').innerHTML=`${playerSelection} player ${computerSelection} computer. ${winner} wins!`;
+
+    }
+    else{
+        document.getElementById('gameResults').innerHTML=`${playerSelection} player ${computerSelection} computer. Draw!`;
+
+    }
+    winner == 'Player' ? playerWins++ : winner=='Computer' ? computerWins++ : winner='draw';
+    document.getElementById("playerScore").innerHTML=`${playerWins}`;
+    document.getElementById("compScore").innerHTML=`${computerWins}`;
+    if(playerWins==5||computerWins==5){
+        endGame(winner);
     }
 }
+
+function endGame(winner){
+    document.getElementById("winner").innerHTML=`${winner} was the first to 5 wins! WOOWOOWOOWOOWOO!`;
+    playerWins=0;
+    computerWins=0;
+}
+//console.log(playRound('ROCK','scissors'));
+document.getElementById("rock").addEventListener('click',playRound);
+document.getElementById("paper").addEventListener('click',playRound);
+document.getElementById("scissors").addEventListener('click',playRound);
+
+
+
+
