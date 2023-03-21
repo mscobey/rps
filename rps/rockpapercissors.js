de2ec3c06c8330a3abe8c619d5ba2d395f0b805c
mscobey <126064568+mscobey@users.noreply.github.com>
2023-03-21 03:55:35 -0500

    js

--- rps/rockpapercissors.js
+++ rps/rockpapercissors.js
@@ -2,7 +2,7 @@ function getComputerChoice(){
     let choices=['rock','paper','scissors'];
     return choices[Math.floor(Math.random()*3)];
 }
-//console.log(getComptuterChoice());
+
 let playerWins=0;
 let computerWins=0;
 
@@ -22,17 +22,20 @@ function playRound(event){
     else{
         playerSelection=='rock'?winner='Player':playerSelection=='paper'?winner='Computer':winner='draw';
     }
+
     if(winner!='draw'){
-        document.getElementById('gameResults').innerHTML=`Player picks ${playerSelection} : Computer picks ${computerSelection} <br> ${winner} wins!`;
+        document.getElementById('gameResults').innerHTML=`Player picks ${playerSelection} : Computer picks ${computerSelection} <br> ${winner} wins the round!`;
 
     }
     else{
         document.getElementById('gameResults').innerHTML=`Player picks ${playerSelection} : Computer picks ${computerSelection} <br> Draw!`;
 
     }
+
     winner == 'Player' ? playerWins++ : winner=='Computer' ? computerWins++ : winner='draw';
     document.getElementById("playerScore").innerHTML=`${playerWins}`;
     document.getElementById("compScore").innerHTML=`${computerWins}`;
+    
     if(playerWins==5||computerWins==5){
         endGame(winner);
     }
@@ -43,7 +46,7 @@ function endGame(winner){
     playerWins=0;
     computerWins=0;
 }
-//console.log(playRound('ROCK','scissors'));
+
 document.getElementById("rock").addEventListener('click',playRound);
 document.getElementById("paper").addEventListener('click',playRound);
 document.getElementById("scissors").addEventListener('click',playRound);
